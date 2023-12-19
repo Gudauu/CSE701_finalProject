d494095cc362c982cd44f6f95443b159d19581f5
Gudauu <gudautd@gmail.com>
2023-12-19 01:22:24 -0500

    try Doxygen

---
+++ demo.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file demo.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+
+/**
+ * @brief 
+ * 
+ * @param a 
+ * @param b 
+ * @return double 
+ */
+double add(const double &a, const double &b)
+{
+    return a + b;
+}
+
+int main()
+{
+    cout << add(1, 2);
+}
